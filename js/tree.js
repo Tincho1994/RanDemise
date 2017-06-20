cbeaed0d7ed04011d93d00d1a0984de7dae3dec7
Brendan Quinn <bjq6@cornell.edu>
2017-06-20 15:27:40 -0400

    code cleanup and starting to add for tree viz

---
+++ js/tree.js
@@ -0,0 +1,3 @@
+/* 		This code will be heavily influenced by: 
+ *  	https://bl.ocks.org/mbostock/4339083
+ */
--- src.js
+++
@@ -1,5 +0,0 @@
-var face=1;
-var rando = function() {
-  face = Math.floor(Math.random() * 6) + 1
-  $('#cube').attr('class', 'show'+face);
-};
